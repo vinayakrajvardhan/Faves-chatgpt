4f493e44647a58dea24b205a0919c5ba19fe1fbf
vinayak raj vardhan <32243863+vinayakrajvardhan@users.noreply.github.com>
2023-02-03 00:11:02 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,4 +3,4 @@ import 'package:flutter/material.dart';
 const backgroundColor = Colors.teal;
 const botBackgroundColor = Color(0xff444654);
 const String apiKeyChatGpt =
-    "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";
+    "api-key";
